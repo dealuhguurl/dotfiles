8edea7932112d9c9898ac32d4b1b713f2cd51719
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2024-02-03 22:37:24 -0800

    nvim

---
+++ nvim/lua/dealuhguurl/init.lua
@@ -0,0 +1,9 @@
+require('dealuhguurl.settings')
+require('dealuhguurl.packer')
+require('dealuhguurl.lsp')
+require('dealuhguurl.cmp')
+require('dealuhguurl.treesitter')
+require('dealuhguurl.auto-pairs')
+require('dealuhguurl.impatient')
+require('dealuhguurl.telescope')
+
---
+++ nvim/lua/dealuhguurl/settings.lua
@@ -0,0 +1,23 @@
+vim.opt.termguicolors = true
+vim.opt.nu = true
+vim.opt.errorbells = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+
+-- ColorScheme
+vim.cmd[[colorscheme catppuccin-mocha]]
+
+vim.cmd[[au FileType * set fo-=c fo-=r fo-=o]]
+--vim.cmd[[set colorcolumn=80]]
+--vim.cmd[[highlight ColorColumn guibg=white]]
