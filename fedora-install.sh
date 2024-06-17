57060d701067b43ca4faf8d46b767c1ead1c45f6
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2024-06-17 00:26:15 -0700

    dnf & icons
    update dnf options & added icons

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,9 +1,9 @@
 # dnf flags
-echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=20' | sudo tee -a /etc/dnf/dnf.conf
+echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
 
 # packages
-sudo dnf install g++ feh neofetch nodejs clang-tools-extra htop curl radare2-devel neovim mingw64-gcc-c++ flatpak flameshot blueman pasystray sqlite kitty keepassxc python3-pip xclip -y -y -y
+sudo dnf install g++ feh neofetch nodejs clang-tools-extra htop curl neovim mingw64-gcc-c++ flatpak flameshot blueman pasystray sqlite kitty keepassxc python3-pip xclip -y -y -y
 
 # flatpak repos
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -16,6 +16,9 @@ sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfre
 # install codecs & drivers
 sudo dnf install mpv akmod-nvidia -y -y
 sudo dnf update -y 
+
+# install icons
+cd && git clone https://github.com/vinceliuice/Tela-icon-theme.git && cd Tela-icon-theme && ./install.sh && cd && rm -r Tela-icon-theme 
 
 # reboot to load proprietary nvidia drivers 
 reboot
