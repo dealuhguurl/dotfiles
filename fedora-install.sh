3bfce320ef22c123ca57b779a565e23ee16bb156
dealuhguurl <dealuhguurl@fedora.attlocal.net>
2024-07-17 23:44:09 -0700

    Update

--- fedora-install.sh
+++ fedora-install.sh
@@ -5,25 +5,27 @@ echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
 # packages
 sudo dnf install g++ feh bswpm polybar sxhkd dunst rofi neofetch nodejs clang-tools-extra htop curl neovim dnf-automatic mingw64-gcc-c++ fontawesome-fonts-all flatpak picom flameshot blueman pasystray pavucontrol sqlite kitty keepassxc python3-pip xclip -y -y -y
 
+# install codecs & drivers
+sudo dnf install mpv akmod-nvidia -y -y
+
 # flatpak repos
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update -y 
+
+# rpm fusion repos
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
 
 # Enable automatic security updates
 echo -e '[commands] \nupgrade_type = security' > automatic.conf
 sudo mv automatic.conf /etc/dnf/
 sudo systemctl enable --now dnf-automatic-install.timer
 
-# rpm fusion repos
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
-
-# install codecs & drivers
-sudo dnf install mpv akmod-nvidia -y -y
-sudo dnf update -y 
-
 # install icons
-cd && git clone https://github.com/vinceliuice/Tela-icon-theme.git && cd Tela-icon-theme && ./install.sh && cd && rm -r Tela-icon-theme -y -y -y
+cd && git clone https://github.com/vinceliuice/Tela-icon-theme.git && cd Tela-icon-theme && ./install.sh && cd && sudo rm -r Tela-icon-theme -y -y -y
+
+# update
+sudo dnf update 
 
 # reboot to load proprietary nvidia drivers 
 reboot
