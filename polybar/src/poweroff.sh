a01735695c0bcd46eb054ddf72c1186018cecbfe
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2024-07-17 20:43:15 -0700

    Update poweroff.sh

--- polybar/src/poweroff.sh
+++ polybar/src/poweroff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 selected=$(echo "Shutdown
-Restart" | rofi -dmenu -p "Power Options")
+Restart" | rofi -dmenu -theme "~/.config/rofi/theme.rasi" -p "Power Options")
  
 if [ "$selected" == "Shutdown" ]
 then
