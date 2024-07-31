481d48ff5f977c68ed19550a64af756d1e4ea149
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2024-07-31 16:41:53 -0700

    Update settings.lua

--- nvim/lua/dealuhguurl/settings.lua
+++ nvim/lua/dealuhguurl/settings.lua
@@ -16,8 +16,7 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 
 -- ColorScheme
--- edit ~/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/lua/tokyonight/colors/night.lua
---vim.cmd[[colorscheme tokyonight-night]]
+--vim.cmd[[colorscheme onedark]]
 
 vim.cmd[[au FileType * set fo-=c fo-=r fo-=o]]
 --vim.cmd[[set colorcolumn=80]]
