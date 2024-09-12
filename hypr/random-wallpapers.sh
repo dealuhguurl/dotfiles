219fd27747a9564bf3e787734f3acc99f3529825
dealuhguurl <dealuhguurl@gmail.com>
2024-09-11 19:29:12 -0700

    .

--- hypr/random-wallpapers.sh
+++ hypr/random-wallpapers.sh
@@ -4,7 +4,7 @@ directory=~/Downloads/wallpapers/
 monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
 
 if [ -d "$directory" ]; then
-    random_background=$(ls $directory/*.jpg | shuf -n 1)
+    random_background=$(ls $directory/*.* | shuf -n 1)
 
     hyprctl hyprpaper unload all
     hyprctl hyprpaper preload $random_background
