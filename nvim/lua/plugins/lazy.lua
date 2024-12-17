934a67a1c08a5a2fee3c49d634bdc6b887b49ac7
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2024-12-16 17:41:14 -0800

    Update lazy.lua

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
     {"hrsh7th/cmp-buffer"},
     {"hrsh7th/cmp-nvim-lsp-signature-help"},
     {"windwp/nvim-autopairs"},
+    {"zbirenbaum/nvterm"},
     {
       "L3MON4D3/LuaSnip",
       dependencies = {
