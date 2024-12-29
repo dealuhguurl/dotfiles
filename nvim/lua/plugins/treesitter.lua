d611b9e96ad952d4248d96e3ed42fcde7fae84d1
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2024-12-28 22:59:32 -0800

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ require("nvim-treesitter.configs").setup {
     -- auto install missing parser
     auto_install = true,
     highlight = {
-        enable = true,
+        enable = false,
         additional_vim_regex_highlighting = false
     },
     incremental_selection = {
