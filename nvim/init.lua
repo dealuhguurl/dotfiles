dece3ec98f7d5ed8aca41ccab779ad2284ea20c3
dealuhguurl <dealuhguurl@gmail.com>
2024-12-30 13:16:01 -0800

    Made vim a global variable

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 opt = vim.opt
 cmd = vim.cmd
 vim.g.mapleader = " "
-
+_G.vim = vim
 require('plugins.init')
 
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -65,10 +65,6 @@ M.defaults = function()
         workspace = {
           library = {
             vim.fn.expand "$VIMRUNTIME/lua",
-            vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-            vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-            vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-            "${3rd}/luv/library",
           },
           maxPreload = 100000,
           preloadFileSize = 10000,
