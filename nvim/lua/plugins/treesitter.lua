f3a7973df772f3ea64ee067363f2ba9ad2409a4f
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2025-01-02 22:02:20 -0800

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup {
     -- auto install missing parser
-    ensure_installed = { "markdown", "markdown_inline" },
-    auto_install = false,
+    ensure_installed = { "markdown", "markdown_inline", "python", "c", "cpp", "rust", "typescript", "javascript" },
+    auto_install = true,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false
