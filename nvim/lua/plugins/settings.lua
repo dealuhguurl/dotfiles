1adfc13e1a1280f52844cf96eabb7bacf6a634d2
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2025-01-08 00:04:31 -0800

    Update settings.lua

--- nvim/lua/plugins/settings.lua
+++ nvim/lua/plugins/settings.lua
@@ -28,3 +28,18 @@ cmd.filetype "plugin indent on"
 --cmd.colorscheme "cyberdream"
 --cmd.colorscheme "monokai"
 
+-- Custom Colorscheme
+vim.cmd('highlight Normal guibg=#16181A guifg=#D1D4D6')
+vim.cmd('highlight Comment guifg=#5B6268')
+vim.cmd('highlight String guifg=#A9B7C6')
+vim.cmd('highlight Keyword guifg=#C792EA')
+vim.cmd('highlight Constant guifg=#FFCB6B')
+vim.cmd('highlight Identifier guifg=#82AAFF')
+vim.cmd('highlight Type guifg=#F78C6C')
+vim.cmd('highlight Function guifg=#C3E88D')
+vim.cmd('highlight LineNr guifg=#4B5263')
+vim.cmd('highlight CursorLine guibg=#2E2E2E')
+vim.cmd('highlight Search guibg=#F2D92F guifg=#16181A')
+vim.cmd('highlight Visual guibg=#3E4451')
+vim.cmd('highlight StatusLine guibg=#2E2E2E guifg=#D1D4D6')
+vim.cmd('highlight StatusLineNC guibg=#16181A guifg=#5B6268')
